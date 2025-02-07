d67482e3c2e380c918404c8800c36e7c66f34ef1
newemail <newemail@example.com>
2025-02-07 13:01:34 +0100

    Film és karakter osztaly elkészult

--- OOPgyakorlas/Program.cs
+++ OOPgyakorlas/Program.cs
@@ -4,16 +4,44 @@
 	{
 		static void Main(string[] args)
 		{
-			konyv konyv1 = new("A kis herceg", "Antione", 1943, 96, 2500);
-			konyv konyv2 = new("Ez még készül", "Én");
+			//konyv konyv1 = new("A kis herceg", "Antione", 1943, 96, 2500);
+			//konyv konyv2 = new("Ez még készül", "Én");
 
-            Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
+   //         Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
 
-			konyv1.Kedvezmeny(20);
-			konyv1.Arnoveles(1000);
+			//konyv1.Kedvezmeny(20);
+			//konyv1.Arnoveles(1000);
 
 
-			Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyOldalMaradt(20));
+			//Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyOldalMaradt(20));
+
+
+			//film film1 = new("Trónók harca", "Valaki", 600, "Epikus", true);
+			//film film2 = new("Lehet", "én");
+
+			//Console.WriteLine(film1.ToString() + "\n" + film2.ToString());
+
+			//film1.MufajModositas("Sorozat");
+			//film2.HosszNoveles(20);
+
+			//film1.Jatszas();
+			//film2.Jatszas();
+
+			//Console.WriteLine(film1.ToString() + "\n" + film2.ToString());
+
+
+			Karakter karak1 = new();
+			Karakter karak2 = new();
+
+			Console.WriteLine(film1.ToString() + "\n" + film2.ToString());
+
+			film1.MufajModositas("Sorozat");
+			film2.HosszNoveles(20);
+
+			film1.Jatszas();
+			film2.Jatszas();
+
+			Console.WriteLine(film1.ToString() + "\n" + film2.ToString());
 
 
 		}
---
+++ OOPgyakorlas/film.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPgyakorlas
+{
+	internal class film
+	{
+		private string cim;
+		private string rendezo;
+		private int HosszPerc;
+		private string mufaj;
+		private bool megjelenes;
+
+		public string Cim { get => cim; set => cim = value; }
+		public string Rendezo { get => rendezo; set => rendezo = value; }
+		public int HosszPerc1 { get => HosszPerc; set => HosszPerc = value; }
+		public string Mufaj { get => mufaj; set => mufaj = value; }
+		public bool Megjelenes { get => megjelenes; set => megjelenes = value; }
+
+		public film(string cim, string rendezo, int hosszPerc, string mufaj, bool megjelenes)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			HosszPerc = hosszPerc;
+			this.mufaj = mufaj;
+			this.megjelenes = megjelenes;
+		}
+
+		public film(string cim, string rendezo)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			HosszPerc = 120;
+			mufaj = "Ismeretlen";
+			megjelenes = false;
+		}
+
+
+		public void Jatszas()
+		{
+            Console.WriteLine($"A(z) \"{cim}\" című film elkezdődött!");
+        }
+
+		 public void MufajModositas(string ujMufaj)
+		{
+			mufaj = ujMufaj;
+		}
+
+		public void HosszNoveles(int percek)
+		{
+			HosszPerc += percek;
+		}
+
+		public override string ToString()
+		{
+			return $"{cim} - {rendezo} / {HosszPerc} / {mufaj} / {megjelenes}";
+		}
+	}
+}
